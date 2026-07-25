e3c7295d72cafdbfb197cdd1be2edc709cd763d3
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-07-24 22:15:06 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 /* global BUILD_INFO */
 import roderuda from './roderuda'
 
+if(typeof roderuda !== "undefined"){
+ window.RodEruda = roderuda;
+}
 console.log('🪅🪄 RodEruda installed! ', BUILD_INFO)
 
 export default roderuda
